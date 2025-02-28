777dc00f822380d2a45376c97a9d0e11ce2ae6fc
harshaltamboli <harshaltamboli429@gmail.com>
2025-02-28 16:21:14 +0530

    this code is create a new server and api

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-exports. sum=(a,b)=>{
-    return a+b;
-}
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const app = express();
 
-exports. minus=(a,b)=>{
-    return a-b;
-}
+const PORT=8002;
 
+
+app.get("/api/users",(req,res)=>{
+    return res.json(users);
+
+});
+
+app.get("/api/users/:id", (req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find((user)=> user.id===id);
+    return res.json(user);
+})
+app.listen(PORT,()=> console.log(`server started at PORT:${PORT}`   )
+);
